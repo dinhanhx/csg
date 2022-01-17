c3c16969026e1f8adcd4376a003bb21f6479a368
Dinh Anh <dinhanhx@gmail.com>
2022-01-17 19:37:08 +0700

    Refactor from Hello to Main

--- src/main/java/usth/based/csg/MainController.java
+++ src/main/java/usth/based/csg/MainController.java
@@ -3,7 +3,7 @@ package usth.based.csg;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class MainController {
     @FXML
     private Label welcomeText;
 
