447820e49219386b8b0b109488b93fd67c11dab7
Dat <tonyvu19997@gmail.com>
2022-01-27 23:17:43 +0700

    comm

--- src/main/java/usth/based/csg/MainApplication.java
+++ src/main/java/usth/based/csg/MainApplication.java
@@ -1,23 +1,26 @@
-package usth.based.csg;
-
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
-import java.io.IOException;
+public class Main extends Application {
 
-public class MainApplication extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    Button button;
+    public static void main(string[]args){
+        launch(args);
     }
 
-    public static void main(String[] args) {
-        launch();
+    public void start(Stage primaryStage) throws Exception{
+        primaryStage.setTitle("CSG Project");
+        button = new Button();
+        button.setText("Click");
+
+        StackPane layout = new StackPane();
+        layout.getChilderen().add(button);
+
+        Scene scene = new  scene(layout, 300, 250);
+        primaryStage.actScene(scene);
+        primaryStage.show();
+
     }
+
 }
