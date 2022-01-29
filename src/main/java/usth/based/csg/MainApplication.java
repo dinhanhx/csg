385b9e5f0ede71a8474d1b6b6214e80564900664
Dat <tonyvu19997@gmail.com>
2022-01-29 14:26:27 +0700

    comm

--- src/main/java/usth/based/csg/MainApplication.java
+++ src/main/java/usth/based/csg/MainApplication.java
@@ -1,26 +1,44 @@
 import javafx.application.Application;
-import javafx.scene.control.Button;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
-public class Main extends Application {
+public class JavaFX_Info extends Application {
+    private double xOffset = 0;
+    private double yOffset = 0;
 
-    Button button;
-    public static void main(string[]args){
-        launch(args);
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("App.fxml"));
+        stage.initStyle(StageStyle.UNDECORATED);
+
+        root.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                xOffset = event.getSceneX();
+                yOffset = event.getSceneY();
+            }
+        });
+
+        root.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                stage.setX(event.getScreenX() - xOffset);
+                stage.setY(event.getScreenY() - yOffset);
+            }
+        });
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.show();
     }
 
-    public void start(Stage primaryStage) throws Exception{
-        primaryStage.setTitle("CSG Project");
-        button = new Button();
-        button.setText("Click");
-
-        StackPane layout = new StackPane();
-        layout.getChilderen().add(button);
-
-        Scene scene = new  scene(layout, 300, 250);
-        primaryStage.actScene(scene);
-        primaryStage.show();
-
+    public static void main(String[] args) {
+        launch(args);
     }
 
 }
