c320a0981f770c9181376bcc245671abff167bfe
NLTD1209 <sunbimbim@gmail.com>
2022-02-11 18:02:45 +0700

    try to write text

--- src/main/java/usth/based/csg/MainApplication.java
+++ src/main/java/usth/based/csg/MainApplication.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.shape.*;
+import javafx.scene.text.*;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,13 +13,19 @@ import java.io.IOException;
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        //Creating a line object
-        Line line = new Line();
-        line.setStartX(100.0);
-        line.setStartY(150.0);
-        line.setEndX(500.0);
-        line.setEndY(150.0);
-        Group root = new Group(line);
+        //Creating a Text object
+        Text text = new Text();
+
+        //Setting font to the text
+        text.setFont(new Font(45));
+
+        //setting the position of the text
+        text.setX(250);
+        text.setY(150);
+
+        //Setting the text to be added.
+        text.setText("USTH");
+        Group root = new Group(text);
 
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("main-view.fxml"));
         //Scene scene = new Scene(fxmlLoader.load(), 320, 240);
