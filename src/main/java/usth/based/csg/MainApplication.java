e8b88a63ce4cd9b505614d6d1d0564684e5a7271
NLTD1209 <sunbimbim@gmail.com>
2022-02-11 22:20:20 +0700

    draw a star

--- src/main/java/usth/based/csg/MainApplication.java
+++ src/main/java/usth/based/csg/MainApplication.java
@@ -13,18 +13,31 @@ import java.io.IOException;
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        //Creating a Polygon
-        Polygon polygon = new Polygon();
+        //Creating a Path
+        Path path = new Path();
 
-        //Adding coordinates to the polygon
-        polygon.getPoints().addAll(new Double[]{
-                300.0, 50.0,
-                450.0, 150.0,
-                300.0, 250.0,
-                150.0, 150.0,
-                230.0, 120.0
-        });
-        Group root = new Group(polygon);
+        //Moving to the starting point
+        MoveTo moveTo = new MoveTo(108, 71);
+
+        //Creating 1st line
+        LineTo line1 = new LineTo(321, 161);
+
+        //Creating 2nd line
+        LineTo line2 = new LineTo(126,232);
+
+        //Creating 3rd line
+        LineTo line3 = new LineTo(232,52);
+
+        //Creating 4th line
+        LineTo line4 = new LineTo(269, 250);
+
+        //Creating 4th line
+        LineTo line5 = new LineTo(108, 71);
+
+        //Adding all the elements to the path
+        path.getElements().add(moveTo);
+        path.getElements().addAll(line1, line2, line3, line4, line5);
+        Group root = new Group(path);
 
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("main-view.fxml"));
         //Scene scene = new Scene(fxmlLoader.load(), 320, 240);
