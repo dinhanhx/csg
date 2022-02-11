c9371c1ca81520e22329f0fedf81f321ffa5a343
NLTD1209 <sunbimbim@gmail.com>
2022-02-11 17:46:52 +0700

    test scene and group

--- src/main/java/usth/based/csg/MainApplication.java
+++ src/main/java/usth/based/csg/MainApplication.java
@@ -2,7 +2,9 @@ package usth.based.csg;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.shape.*;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,8 +12,17 @@ import java.io.IOException;
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        //Creating a line object
+        Line line = new Line();
+        line.setStartX(100.0);
+        line.setStartY(150.0);
+        line.setEndX(500.0);
+        line.setEndY(150.0);
+        Group root = new Group(line);
+
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        //Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(root, 600,300);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
