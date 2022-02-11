62242f3081fc162cdff235cfdd05a5afd626e848
NLTD1209 <sunbimbim@gmail.com>
2022-02-11 23:06:16 +0700

    draw a 3D box/cube

--- src/main/java/usth/based/csg/MainApplication.java
+++ src/main/java/usth/based/csg/MainApplication.java
@@ -3,9 +3,11 @@ package usth.based.csg;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
+import javafx.scene.PerspectiveCamera;
 import javafx.scene.Scene;
 import javafx.scene.shape.*;
 import javafx.scene.text.*;
+import javafx.scene.transform.*;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,35 +15,28 @@ import java.io.IOException;
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        //Creating a Path
-        Path path = new Path();
+        //Drawing a Box
+        Box box = new Box();
 
-        //Moving to the starting point
-        MoveTo moveTo = new MoveTo(108, 71);
+        //Setting the properties of the Box
+        box.setWidth(80.0);
+        box.setHeight(50.0);
+        box.setDepth(100.0);    
 
-        //Creating 1st line
-        LineTo line1 = new LineTo(321, 161);
-
-        //Creating 2nd line
-        LineTo line2 = new LineTo(126,232);
-
-        //Creating 3rd line
-        LineTo line3 = new LineTo(232,52);
-
-        //Creating 4th line
-        LineTo line4 = new LineTo(269, 250);
-
-        //Creating 4th line
-        LineTo line5 = new LineTo(108, 71);
-
-        //Adding all the elements to the path
-        path.getElements().add(moveTo);
-        path.getElements().addAll(line1, line2, line3, line4, line5);
-        Group root = new Group(path);
+        Translate translate = new Translate(250,100,0);
+        Rotate rotate = new Rotate(30,250,100);
+        box.getTransforms().addAll(translate);
+        Group root = new Group(box);
 
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("main-view.fxml"));
         //Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         Scene scene = new Scene(root, 600,300);
+        //Setting camera
+        PerspectiveCamera camera = new PerspectiveCamera(false);
+        camera.setTranslateX(0);
+        camera.setTranslateY(0);
+        camera.setTranslateZ(0);
+        scene.setCamera(camera);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
